5616f661e50edd3b9fcd0eac9957edef7ef4095c
Notebook <mhmd.amin.faridfar@gmail.com>
2025-07-10 00:44:22 +0330

    complete the show menu

--- simple calculater.py
+++ simple calculater.py
@@ -1,1 +1,15 @@
-print("test commit")
+def show_menu() :
+    print("*** Simple Calculater ***")
+    print("1. String of (+ , - , * , /)")
+    print("2. Power")
+    print("3. Root")
+    print("4. Percentage (%)")
+    print("5. Factorial (!)")
+    print("6. Abs")
+    print("Enter your choice :")
+    b = int(input())
+    return b 
+
+ 
+a = show_menu()
+print(a)#testing the define
